7416a4ad9683d786454c30dd7063122fe9193038
Anastasia Mamatova <mamatovaanastasia31@gmail.com>
2025-11-21 20:54:02 +0200

    add basic styles for booking form

--- components/sections/BookingForm/BookingForm.tsx
+++ components/sections/BookingForm/BookingForm.tsx
@@ -1,5 +1,55 @@
 'use client';
+import css from "./BookingForm.module.css";
 
 export default function BookingForm() {
-  return <form>Booking Form</form>;
+  return <form className={css.bookingForm}>
+    <div className={css.headerBlock}>
+  <h3 className={css.title}>Book your campervan now</h3>
+  <p className={css.subtitle}>Stay connected! We are always ready to help you.</p>
+    </div>
+    
+    <div className={css.fieldsBlock}>
+  <label className={css.label}>
+    <input
+      type="text"
+      name="name"
+      required
+      className={css.input}
+      placeholder="Name*"
+    />
+  </label>
+
+  <label className={css.label}>
+    <input
+      type="email"
+      name="email"
+      required
+      className={css.input}
+      placeholder="Email*"
+    />
+  </label>
+
+  <label className={css.label}>
+    <input
+      type="date"
+      name="date"
+      required
+      className={css.input}
+      placeholder="Booking date*"
+    />
+  </label>
+
+  <label className={css.label}>
+    <textarea
+      name="comment"
+      className={css.textarea}
+      rows={4}
+      placeholder="Comment"
+    ></textarea>
+  </label>
+
+</div>
+  <button type="submit" className={css.sendBtn}>Send</button>
+</form>
+
 }
